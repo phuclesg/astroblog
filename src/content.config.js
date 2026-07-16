08bdf50c78594afca14f4fdb6d1a749bf5dc4b96
Phuc Le <phucle@Phucs-MacBook-Air.local>
2026-07-16 12:53:35 +0700

    update

--- src/content.config.js
+++ src/content.config.js
@@ -18,6 +18,7 @@ const blog = defineCollection({
       tags: z.array(z.string()).default([]),
       author: z.string(),
       thumbnail: image(),
+      readingTime: z.number(),
       imageCredit: z
         .object({
           caption: z.string().optional(),
